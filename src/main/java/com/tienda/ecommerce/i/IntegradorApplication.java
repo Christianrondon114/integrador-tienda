328f36f10ed01eb786f9932187d7d17741247f27
Ronaldinho <Christianrondon114@gmail.com>
2025-03-27 09:15:28 -0500

    Primer Commit - Configuración Springboot, GIT, inicialización de clases 'modelo'

---
+++ src/main/java/com/tienda/ecommerce/i/IntegradorApplication.java
@@ -0,0 +1,14 @@
+package com.tienda.ecommerce.i;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class IntegradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegradorApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/tienda/ecommerce/i/model/Product.java
@@ -0,0 +1,95 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tienda.ecommerce.i.model;
+
+/**
+ *
+ * @author Ronaldinho
+ */
+public class Product {
+    private Integer id;
+    private String nombre;
+    private String descripcion;
+    private String imagen;
+    private double precio;
+    private int cantidad;
+    private boolean disponible;
+    private int stock;
+
+    public Product() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" + "id=" + id + ", nombre=" + nombre + ", descripcion=" + descripcion + ", precio=" + precio + ", cantidad=" + cantidad + ", stock=" + stock + ", imagen=" + imagen + ", disponible=" + disponible + '}';
+    }
+
+        
+    
+}
