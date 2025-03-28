84dd3987fefc2cfd34f04b514ed25f8a29d16d52
Ronaldinho <Christianrondon114@gmail.com>
2025-03-27 22:34:03 -0500

    Conexión con la base de datos Springboot

--- src/main/java/com/tienda/ecommerce/i/IntegradorApplication.java
+++ src/main/java/com/tienda/ecommerce/i/IntegradorApplication.java
@@ -2,9 +2,8 @@ package com.tienda.ecommerce.i;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class IntegradorApplication {
 
 	public static void main(String[] args) {
